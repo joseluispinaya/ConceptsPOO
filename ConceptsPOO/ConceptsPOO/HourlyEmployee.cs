ed320b38f8a77aeae3142b15ae0b7aecccf730b4
JOSE LUIS PINAYA BALDERRAMA <jpinayab@doc.emi.edu.bo>
2022-03-24 01:40:11 -0400

    swgunda clase

---
+++ ConceptsPOO/ConceptsPOO/HourlyEmployee.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConceptsPOO
+{
+    public class HourlyEmployee : Employee
+    {
+        public float Hours { get; set; }
+
+        public decimal HourValue { get; set; }
+
+        public override decimal GetValueToPay()
+        {
+            return (decimal)Hours * HourValue;
+        }
+        public override string ToString()
+        {
+            return $"{base.ToString()}" +
+                $"\n\tHours................: {$"{Hours:N2}",18}" +
+                $"\n\tHour value...........: {$"{HourValue:C2}",18}" +
+                $"\n\tValue to pay.........: {$"{GetValueToPay():C2}",18}";
+        }
+    }
+}
--- ConceptsPOO/ConceptsPOO/Program.cs
+++ ConceptsPOO/ConceptsPOO/Program.cs
@@ -4,18 +4,57 @@
 using ConceptsPOO;
 
 Console.WriteLine("Hello, World!");
+Employee employee1 = new SalaryEmployee()
+{
+    Id = 1010,
+    FirstName = "Sandra",
+    LastName = "Morares",
+    BirthDate = new Date(1990, 5, 23),
+    HiringDate = new Date(2022, 1, 15),
+    IsActive = true,
+    Salary = 1815453.45M
+};
+//Console.WriteLine(employee1);
 
-try
+Employee employee2 = new CommissionEmployee()
 {
-    Console.WriteLine("==============");
-    Console.WriteLine(new Date(2024, 2, 29));
-    Console.WriteLine("==============");
-    Console.WriteLine(new Date(1974, 9, 23));
-    Console.WriteLine("==============");
-    Console.WriteLine(new Date(1985, 11, 30));
-}
-catch (Exception error)
+    Id = 2020,
+    FirstName = "Patricia",
+    LastName = "Gutierrez",
+    BirthDate = new Date(1995, 5, 23),
+    HiringDate = new Date(2022, 2, 5),
+    IsActive = true,
+    Sales = 320000000M,
+    CommissionPercentaje = 0.03F
+};
+//Console.WriteLine(employee2);
+
+Employee employee3 = new HourlyEmployee()
 {
-    Console.WriteLine(error.Message);
-}
+    Id = 3030,
+    FirstName = "Gonzalo",
+    LastName = "Cardona",
+    BirthDate = new Date(1985, 5, 23),
+    HiringDate = new Date(2022, 2, 5),
+    IsActive = true,
+    HourValue = 12356.56M,
+    Hours = 123.5F
+};
+Console.WriteLine(employee3);
+
+
+
+//try
+//{
+//    Console.WriteLine("==============");
+//    Console.WriteLine(new Date(2024, 2, 29));
+//    Console.WriteLine("==============");
+//    Console.WriteLine(new Date(1974, 9, 23));
+//    Console.WriteLine("==============");
+//    Console.WriteLine(new Date(1985, 11, 30));
+//}
+//catch (Exception error)
+//{
+//    Console.WriteLine(error.Message);
+//}
 
